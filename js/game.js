50c3079f154e939db9480ab892b38aae8677d5f7
Nero Denney <penance316@gmail.com>
2017-07-05 12:05:06 +0100

    setup of some basic game elements

--- js/game.js
+++ js/game.js
@@ -1,28 +1,47 @@
 var gameProperties = {
     screenWidth: 640,
     screenHeight: 480,
+
+    tileWidth: 32,
+    tileHeight: 32,
+
+    boardWidth: 9,
+    boardHeight: 9,
+
+    totalMines: 10,
 };
 
 var states = {
     game: "game",
 };
 
-var gameState = function(game){
-    
+var graphicAssets = {
+    tiles: { URL: 'assets/tiles.png', name: 'tiles', frames: 14 },
+};
+
+var gameState = function(game) {
+    this.boardTop;
+    this.boardLeft;
+    this.board;
 };
 
 gameState.prototype = {
-    
-    preload: function () {
-        
-    },
-    
-    create: function () {
-        
+
+    init: function() {
+        this.boardTop = (gameProperties.screenHeight - (gameProperties.tileHeight * gameProperties.boardHeight)) * 0.5;
+        this.boardLeft = (gameProperties.screenWidth - (gameProperties.tileWidth * gameProperties.boardWidth)) * 0.5;
     },
 
-    update: function () {
-        
+    preload: function() {
+        game.load.spritesheet(graphicAssets.tiles.name, graphicAssets.tiles.URL, gameProperties.tileWidth, gameProperties.tileHeight, graphicAssets.tiles.frames);
+    },
+
+    create: function() {
+
+    },
+
+    update: function() {
+
     },
 };
 
